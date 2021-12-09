52b53a7706e912190fae6f9914b18617704d0794
matildegarelik <garelikmatilde@gmail.com>
2021-12-09 19:16:14 -0300

    eliminada clase Fiados

--- GestionKiosco/Fiados.cpp
+++
@@ -1,6 +0,0 @@
-#include "Fiados.h"
-
-Fiados::Fiados() {
-	
-}
-
--- GestionKiosco/Venta.h
+++ GestionKiosco/Venta.h
@@ -5,28 +5,29 @@
 
 struct Detalle{
 	Producto p;
-	int cantidad, fecha;
-};
-
-struct Fiado{
-	Detalle d;
-	string cliente;
-	bool pagado=false;
+	int cantidad;
 };
 
 class Venta {
 public:
 	Venta();
 	int VerCantidad(int codigo);
-	void AgregarVenta(int codigo, int cantidad);
-	void EliminarVenta(int indice);
+	void AgregarDetalle(int codigo, int cantidad);
+	void EliminarDetalle(int indice);
 	float CalcularTotal();
 	void Pagar(Productos &p); // tambien actualiza stock en vector de productos
 	void Fiar(string cliente, string nombre_archivo);
-	void GuardarVenta(string nombre_archivo); //binario
+	void GuardarVenta(string archivo_ventas, string archivo_fiados); //binario
+	
+	void CargarFiados(string archivo_fiados); //archivo binario
+	void MarcarPagado(int indice); //guarda vector y archivos
+	void Ordenar();
+	
 private:
-	vector<Producto> _prods;
-	vector<int> _cantidades;
+	vector<Detalle> _detalles;
+	char _cliente[50];
+	bool _pagado;
+	int _fecha;
 };
 
 #endif
