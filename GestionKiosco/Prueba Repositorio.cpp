951b5ff0dee781710dd5f65c1d256985e23cd7d4
matildegarelik <garelikmatilde@gmail.com>
2021-12-21 12:41:28 -0300

    error en ultimas ventas

--- GestionKiosco/Prueba Repositorio.cpp
+++ GestionKiosco/Prueba Repositorio.cpp
@@ -7,36 +7,42 @@
 #include <iostream>
 using namespace std;
 
-/*int main() {
-	Repositorio<Producto> repo_produtos("productos.dat");
+int main() {
+//	Repositorio<Producto> repo_produtos("productos.dat");
 //	Venta v1;
 //	v1.repo_ventas.cargarDatosDePruebaProducto
 //	Repositorio<Compra> repo_ventas2("ventas.dat");
 //
 //	repo_produtos.cargarDatosDePruebaProducto();
-	vector<Producto> v = repo_produtos.buscarTodos();
+//	vector<Producto> v = repo_produtos.buscarTodos();
 //
-	repo_produtos.verTodos();
-	cout<<endl;
+//	repo_produtos.verTodos();
+//	cout<<endl;
 //	
-	Producto nuevo = {"barra frutal", "arcor", "alimento", 006, 4, 100};
-	repo_produtos.guardarNuevo(nuevo);
+//	Producto nuevo = {"barra frutal", "arcor", "alimento", 006, 4, 100};
+//	repo_produtos.guardarNuevo(nuevo);
 //	
-	repo_produtos.verTodos();
-	cout<<endl;
+//	repo_produtos.verTodos();
+//	cout<<endl;
 //	
 //
-	repo_produtos.eliminarPermanente(nuevo);
-	repo_produtos.verTodos();
+//	repo_produtos.eliminarPermanente(nuevo);
+//	repo_produtos.verTodos();
 //	repo_ventas.cargarDatosDePruebaVenta();
 //	Compra nueva = {"cccc", 3,10,2020,300}:
 //	repo_ventas.guardarNuevo(nueva);
 //	repo_ventas.eliminarPermanente(nueva);
+	Productos prods;
+	Venta v;
+	//vector<Compra> ults = v.ultimas_ventas(prods);
+	vector<Compra> ults;
+	Compra x={"matilde\0",Fecha{21,12,2021},100};
+	ults.push_back(x);
+	for(Compra c: ults) cout<<c.total<<endl;
 	
 	
-	
-	int x;
-	cin>>x;
+	int f;
+	cin>>f;
 	return 0;
-}*/
+}
 	
